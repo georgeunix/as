10fdb5a10371f7f4aef45850c845ecd3db9ca410
unknown <asantiago@PC740899502009.produccion.gob.pe>
2013-08-22 10:57:30 -0500

    mensaje_asc

---
+++ src/Produce/DiacBundle/Util/AjaxController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Produce\DiacBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Alert of AjaxController
+ *
+ * @author Alex  Santiago
+ */
+class AjaxController extends Controller {
+    
+    
+    
+    
+}
+
+
+
+
+?>
